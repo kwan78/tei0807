3fb2ba3b9a3e3a53b9184ad60ce5918a68e9b23b
kwan78 <tei_tokyo@naver.com>
2012-08-07 14:52:03 +0900

    alam project

---
+++ AlamAdd.h
@@ -0,0 +1,13 @@
+//
+//  AlamAdd.h
+//  tei_test2
+//
+//  Created by akaon on 12. 8. 7..
+//  Copyright (c) 2012년 akaon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AlamAdd : NSObject
+
+@end
---
+++ AlamAdd.m
@@ -0,0 +1,13 @@
+//
+//  AlamAdd.m
+//  tei_test2
+//
+//  Created by akaon on 12. 8. 7..
+//  Copyright (c) 2012년 akaon. All rights reserved.
+//
+
+#import "AlamAdd.h"
+
+@implementation AlamAdd
+
+@end
